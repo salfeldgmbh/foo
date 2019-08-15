72d25732476bc7352f7367680786f1de4fe5dc8f
salfeldgmbh <ds@salfeld.de>
2019-08-15 17:23:20 +0200

    initial commit

---
+++ Controllers/FooController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Foo4API.Controllers
+{
+    [RoutePrefix("api/foo")]
+
+    public class FooController : ApiController
+    {
+
+        [HttpGet, Route("hello")]
+        public IHttpActionResult CltLoginByToken()
+        {
+            return Ok("bar rocks");
+        }
+
+    }
+}
+
